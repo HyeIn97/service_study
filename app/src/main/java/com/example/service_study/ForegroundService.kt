e770c58f3a31a160f8d3baba9ce614569135c40f
HyeIn97 <wslx97@naver.com>
2024-09-06 14:09:53 +0900

    [style] 변수명을 수정한다.

--- app/src/main/java/com/example/service_study/ForegroundService.kt
+++ app/src/main/java/com/example/service_study/ForegroundService.kt
@@ -13,7 +13,7 @@ class ForegroundService : Service() {
     private val CHANNEL_ID = "CHANNEL_STUDY"
 
     private var notificationManager: NotificationManager? = null
-    private var notificationBuilder: Notification? = null
+    private var notification: Notification? = null
 
     override fun onBind(intent: Intent?): IBinder? = null
 
@@ -27,12 +27,12 @@ class ForegroundService : Service() {
     private fun createNotification() {
         notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
         notificationManager?.createNotificationChannel(NotificationChannel(CHANNEL_ID, "CHANNEL_NAME", NotificationManager.IMPORTANCE_NONE))
-        notificationBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+        notification = NotificationCompat.Builder(this, CHANNEL_ID)
             .setSmallIcon(R.drawable.ic_launcher_foreground)
             .setContentTitle("Service Study Notification")
             .setContentText("~ Notification Content ~")
             .build()
 
-        startForeground(SERVICE_ID, notificationBuilder)
+        startForeground(SERVICE_ID, notification)
     }
 }
