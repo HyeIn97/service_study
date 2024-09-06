cf7442de67b5f673ae6f5caeefaaad5dc58cece3
HyeIn97 <wslx97@naver.com>
2024-09-06 14:11:49 +0900

    [refactor] activity와 view를 연결하고 클릭 이벤트를 추가한다.

--- app/src/main/java/com/example/service_study/MainActivity.kt
+++ app/src/main/java/com/example/service_study/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.service_study
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.service_study.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initListener()
+    }
+
+    private fun initListener() = with(binding) {
+        startButton.setOnClickListener {
+            startForegroundService(Intent(this@MainActivity, ForegroundService::class.java))
+        }
+
+        stopButton.setOnClickListener {
+            stopService(Intent(this@MainActivity, ForegroundService::class.java))
+        }
     }
 }
